409484237fb25762d289c36d8f84c3855d4bdd84
Rom0105 <savosko.photo@gmail.com>
2021-05-24 13:58:58 +0300

    правки

--- js/task-02.js
+++ js/task-02.js
@@ -7,26 +7,9 @@ const ingredients = [
   "Приправы",
 ];
 
-const listDoc = document.querySelector("#ingredients");
-
-const itemDocPotato = document.createElement("li");
-itemDocPotato.textContent = "Картошка";
-const itemDocMushrooms = document.createElement("li");
-itemDocMushrooms.textContent = "Грибы";
-const itemDocGarlic = document.createElement("li");
-itemDocGarlic.textContent = "Чеснок";
-const itemDocTomatoes = document.createElement("li");
-itemDocTomatoes.textContent = "Помидоры";
-const itemDocGreens = document.createElement("li");
-itemDocGreens.textContent = "Зелень";
-const itemDocCondiments = document.createElement("li");
-itemDocCondiments.textContent = "Приправы";
-
-listDoc.append(
-  itemDocPotato,
-  itemDocMushrooms,
-  itemDocGarlic,
-  itemDocTomatoes,
-  itemDocGreens,
-  itemDocCondiments
-);
+let listDoc = document.querySelector("#ingredients");
+const foodDoc = ingredients.forEach((ingredient) => {
+  let items = document.createElement("li");
+  items.innerHTML = ingredient;
+  listDoc.append(items);
+});
