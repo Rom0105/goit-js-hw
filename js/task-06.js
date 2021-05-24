0062b40bcccf85055a30c8a5adf4540d31b189c9
Rom0105 <savosko.photo@gmail.com>
2021-05-24 19:36:23 +0300

    правки

--- js/task-06.js
+++ js/task-06.js
@@ -1,30 +1,13 @@
-const validationDoc = Number(length);
-document.querySelector("#validation-input").onblur = function () {
-  if (this.getAttribute("data-length") > validationDoc) {
-    this.classList.remove("valid");
-    this.classList.add("invalid");
-  } else {
-    this.classList.remove("invalid");
-    this.classList.add("valid");
-  }
-};
-
-/* let inputDoc = document.querySelector("#validation-input");
-
-let lenghtDoc = inputDoc.getAttribute("data-length");
-let totallenghtDoc = parseInt(lenghtDoc, 10);
-
-inputDoc.oninput = function () {
-  if (inputDoc.value.length === totallenghtDoc) {
-    inputDoc.classList.remove("invalid");
-    inputDoc.classList.add("valid");
-  }
-  if (inputDoc.value.length === 0) {
-    inputDoc.classList.remove("valid");
-    inputDoc.classList.remove("invalid");
-  }
-  if (inputDoc.value.length !== totallenghtDoc && inputDoc.value.length !== 0) {
-    inputDoc.classList.add("invalid");
-  }
-};
- */
+const validationDoc = document.querySelector("#validation-input");
+validationDoc.addEventListener("focus", onInputFocus);
+validationDoc.addEventListener("blur", onInputBlur);
+function onInputFocus(event) {
+  validationDoc.classList.remove("valid");
+  validationDoc.classList.remove("invalid");
+}
+function onInputBlur(event) {
+  Number(event.currentTarget.value.length) ===
+  Number(validationDoc.dataset.length)
+    ? validationDoc.classList.add("valid")
+    : validationDoc.classList.add("invalid");
+}
